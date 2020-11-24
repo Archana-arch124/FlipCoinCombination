bb12ff1244881a3b48e705c3d1dc09b1bd95200c
unknown <#archgowda124@gmail.com>
2020-11-24 17:47:51 +0530

    Display head or tail

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,2 +1,12 @@
-Welcome to FlipCoin Combination Problem
+#Welcome to FlipCoin Combination Problem
+
+#!/bin/bash
+
+   flip=$((RANDOM%2))
+   if [ $flip -eq 1 ]
+   then
+     echo "Heads win"
+   else
+      echo "Tails win"
+   fi
 
